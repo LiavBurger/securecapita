e7213e6a28013aee25e7463ce492bf876eba8bd4
Liav Burger <liav.burger22@Gmail.com>
2023-11-20 23:24:33 +0200

    Implemented User and Role repositories

---
+++ src/main/java/com/liavb/securecapita/repository/RoleRepository.java
@@ -0,0 +1,20 @@
+package com.liavb.securecapita.repository;
+
+import com.liavb.securecapita.domain.Role;
+
+import java.util.Collection;
+
+public interface RoleRepository<T extends Role> {
+    /* Basic CRUD Operations */
+    T create(T data);
+    Collection<T> list(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    boolean delete(Long id);
+
+    /* More Complex Operations */
+    void addRoleToUser(Long userId, String roleName);
+    Role getRoleByUserId(Long userId);
+    Role getRoleByUserEmail(String email);
+    void updateUserRole(Long userId, String roleName);
+}
---
+++ src/main/java/com/liavb/securecapita/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.liavb.securecapita.repository;
+
+import com.liavb.securecapita.domain.User;
+
+import java.util.Collection;
+
+public interface UserRepository<T extends User> {
+    /* Basic CRUD Operations */
+    T create(T data);
+    Collection<T> list(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    boolean delete(Long id);
+
+    /* More Complex Operations */
+}
